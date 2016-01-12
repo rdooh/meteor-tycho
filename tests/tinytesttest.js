54bc7a0014d513251746ad5908c7c2915015cd5b
rdooh <robdooh@Robs-WM-MacBook-Pro.local>
2016-01-12 08:16:56 -0500

    some basic tests but failing to set type

--- tests/tinytesttest.js
+++ tests/tinytesttest.js
@@ -1,3 +1,54 @@
-Tinytest.add('MeteorTycho - Trivial test', (test) => {
-  test.isTrue(true,'true is true');
+
+// Set up a simple base class
+const TychoTest = Astro.Class({
+  name: 'Tycho',
+  typeField: 'type',
+  fields: {
+    '_eventStream': 'array',
+    'name': 'string'
+  }
+});
+
+
+const Items = new Mongo.Collection('items');
+// Inherit as you might for
+const KeplerTest = TychoTest.inherit({
+  name: 'Kepler',
+  collection: Items,
+  fields: {
+    someField: 'string'
+  }
+});
+
+const tycho = new TychoTest();
+const kepler = new KeplerTest();
+
+
+
+
+
+const Observation = Astro.Class({
+  name: 'Observations'
+});
+
+
+// Run basic tests on Tycho
+Tinytest.add('Tycho - Basic Fields tests', (test) => {
+
+  test.equal(TychoTest.getFieldsNames(),['_eventStream','name'],'TychoTest should have a `name` field.');
+  // test.equal(tycho.type,'TychoTest','TychoTest should be `TychoTest`','boo');
+  // console.log("message");
+  // console.log(tycho);
+
+
+});
+
+
+
+Tinytest.add('Kepler - Basic Kepler tests', (test) => {
+  console.log("message");
+  console.log(kepler);
+  test.equal(KeplerTest.getFieldsNames(),["_eventStream","name","_id","someField"],'Kepler should have a expected fields.');
+  test.equal(kepler.type,'KeplerTest','KeplerTest should be `KeplerTest`');
+
 });
